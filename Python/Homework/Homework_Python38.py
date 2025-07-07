803a6c98d09490e562975cb8e8367538d1bd7e22
irynaPL99 <iryna.platonova99@gmail.com>
2025-07-07 21:55:24 +0200

    added new file Homework_Python38.py

---
+++ Python/Homework/Homework_Python38.py
@@ -0,0 +1,79 @@
+'''Task 1. Банковский счёт
+Создайте класс BankAccount, описывающий банковский счёт.
+Объект должен хранить имя владельца и текущий баланс.
+Реализуйте методы:
+-пополнение счёта
+-снятие средств
+-отображение баланса
+При попытке снять больше, чем есть на счёте,
+операция не должна выполняться.
+Продумайте, какие поля и методы следует скрыть от внешнего доступа,
+а какие оставить открытыми.
+'''
+
+'''Task 2. История операций
+Доработайте класс BankAccount.
+Каждая операция пополнения и снятия должна сохраняться в историю.
+История должна быть доступна через property history только для чтения.
+История представляется в виде списка строк ("Deposit: 150", "Withdraw: 100" и т.д.).
+'''
+print("\n Task 1. Банковский счёт")
+class BankAccount:
+    def __init__(self, name_owner : str, current_balance: float):
+        self.name_owner = name_owner
+        self._current_balance = current_balance # чтобы нельзя было напрямую менять извне.
+        self._history = []  # скрытое поле для истории операций (чтобы нельзя было перезаписать.)
+
+     # Строковое представление: (username: Alice, password: 1234, num: 1)
+    #def __str__(self):
+    #    return f"account info: {self.name_owner}, Current balance: {self._current_balance}, history: {self._history}"
+
+    def show_balance(self):
+        print(f"Current balance: {self._current_balance}")
+
+    def add_deposit(self, amount: float):
+        if amount <= 0:
+            print("Error: Amount must be positive.")
+            return
+
+        self._current_balance += amount
+        self._history.append(f"Deposit: {amount}")
+
+    def withdraf_deposit(self, amount):
+        if amount <= 0:
+            print("Error: Amount must be positive.")
+            return
+
+        if amount > self._current_balance:
+            print("Error: Not enough funds.")
+            return
+
+        self._current_balance -= amount
+        self._history.append(f"Withdraw: {amount}")
+
+
+    # История должна быть доступна через property history только для чтения.
+    @property
+    def history_print(self):
+        return self._history.copy()
+
+# main code
+account = BankAccount("Alica", 150)
+account.show_balance()
+
+account.add_deposit(-1.0) # "Error: Amount must be positive."
+account.show_balance()
+
+account.add_deposit(1.0)
+account.show_balance()
+
+account.withdraf_deposit(200.0) # "Error: Not enough funds."
+account.show_balance()
+
+account.withdraf_deposit(100.0)
+account.show_balance()
+
+#print(account)
+print("\nOperation history:")
+for operation in account.history_print:
+    print("\t" + operation)
